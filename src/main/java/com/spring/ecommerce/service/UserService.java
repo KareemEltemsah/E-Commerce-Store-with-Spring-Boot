6d739c6837bf161412e3957d374c0df64c0b9271
Kareem Eltemsah <36171346+KareemEltemsah@users.noreply.github.com>
2023-09-07 01:25:33 +0300

    add password encoder bean and encode password

--- src/main/java/com/spring/ecommerce/service/UserService.java
+++ src/main/java/com/spring/ecommerce/service/UserService.java
@@ -5,12 +5,14 @@ import com.spring.ecommerce.exception.UserAlreadyExistException;
 import com.spring.ecommerce.model.LocalUser;
 import com.spring.ecommerce.model.dao.UserRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
 @RequiredArgsConstructor
 public class UserService {
     private final UserRepository userRepository;
+    private final PasswordEncoder encryptionService;
 
     public LocalUser registerUser(RegistrationBody registrationBody) throws UserAlreadyExistException {
         if (userRepository.findByUsername(registrationBody.getUsername()).isPresent()
@@ -23,7 +25,7 @@ public class UserService {
         user.setEmail(registrationBody.getEmail());
         user.setFirstName(registrationBody.getFirstName());
         user.setLastName(registrationBody.getLastName());
-        user.setPassword(registrationBody.getPassword());
+        user.setPassword(encryptionService.encode(registrationBody.getPassword()));
 
         return userRepository.save(user);
     }

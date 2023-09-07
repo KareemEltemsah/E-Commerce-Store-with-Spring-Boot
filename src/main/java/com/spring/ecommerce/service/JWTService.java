3c24f14a5b32a4c70464001446d1984c6080fa2b
Kareem Eltemsah <36171346+KareemEltemsah@users.noreply.github.com>
2023-09-07 05:02:45 +0300

    add JWT service with function to generate jwt token

---
+++ src/main/java/com/spring/ecommerce/service/JWTService.java
@@ -0,0 +1,32 @@
+package com.spring.ecommerce.service;
+
+import com.auth0.jwt.JWT;
+import com.auth0.jwt.algorithms.Algorithm;
+import com.spring.ecommerce.model.LocalUser;
+import jakarta.annotation.PostConstruct;
+import org.springframework.stereotype.Service;
+
+import java.util.Date;
+
+@Service
+public class JWTService {
+
+    private static final String algorithmKey = "SuperSecureSecretKey";
+    private static final String issuer = "eCommerce";
+    private static final Long expiryInSeconds = 604800L;
+
+    private Algorithm algorithm;
+
+    @PostConstruct
+    public void postConstruct() {
+        algorithm = Algorithm.HMAC256(algorithmKey);
+    }
+
+    public String generateJwtToken(LocalUser user) {
+        return JWT.create()
+                .withSubject(user.getUsername())
+                .withExpiresAt(new Date(System.currentTimeMillis() + (1000 * expiryInSeconds)))
+                .withIssuer(issuer)
+                .sign(algorithm);
+    }
+}

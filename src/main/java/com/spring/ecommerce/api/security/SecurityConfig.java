76453c5a911c21c3818b07b1dc66afa586d39640
Kareem Eltemsah <36171346+KareemEltemsah@users.noreply.github.com>
2023-09-09 20:57:10 +0300

    authorize http requests

--- src/main/java/com/spring/ecommerce/api/security/SecurityConfig.java
+++ src/main/java/com/spring/ecommerce/api/security/SecurityConfig.java
@@ -1,20 +1,27 @@
 package com.spring.ecommerce.api.security;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
+@RequiredArgsConstructor
 public class SecurityConfig {
+    private final JWTAuthenticationFilter JwtAuthFilter;
+
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http.csrf(AbstractHttpConfigurer::disable);
         http.cors(AbstractHttpConfigurer::disable);
+        http.addFilterBefore(JwtAuthFilter, UsernamePasswordAuthenticationFilter.class);
         http.authorizeHttpRequests(
                 configurer -> configurer
-                        .anyRequest().permitAll()
+                        .requestMatchers("/auth/register", "/auth/login", "/product").permitAll()
+                        .anyRequest().authenticated()
         );
 
         return http.build();

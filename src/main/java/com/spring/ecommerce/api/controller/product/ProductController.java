573fe86281d3d7821e2a67ba514143b8638d4ba8
Kareem Eltemsah <36171346+KareemEltemsah@users.noreply.github.com>
2023-09-09 17:34:34 +0300

    add endpoint to get all products

--- src/main/java/com/spring/ecommerce/api/controller/product/ProductController.java
+++ src/main/java/com/spring/ecommerce/api/controller/product/ProductController.java
@@ -1,13 +1,21 @@
 package com.spring.ecommerce.api.controller.product;
 
+import com.spring.ecommerce.model.Product;
 import com.spring.ecommerce.service.ProductService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/product")
 @RequiredArgsConstructor
 public class ProductController {
     private final ProductService productService;
+
+    @GetMapping public List<Product> getProducts() {
+        return productService.getProducts();
+    }
 }
--- src/main/java/com/spring/ecommerce/service/ProductService.java
+++ src/main/java/com/spring/ecommerce/service/ProductService.java
@@ -5,9 +5,15 @@ import com.spring.ecommerce.model.dao.ProductRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
 @RequiredArgsConstructor
 public class ProductService {
 
     private final ProductRepository productRepository;
+
+    public List<Product> getProducts() {
+        return productRepository.findAll();
+    }
 }
